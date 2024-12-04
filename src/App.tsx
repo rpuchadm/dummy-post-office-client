6e9335285240ae45e4818fc53870b119bd63788e
Rafael Puchades <rpuchadm@cfp.upv.es>
2024-12-04 20:29:35 +0100

    react-routerv6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import Layout from "./components/layout/Layout"
 import HomePage from "./components/HomePage"
@@ -9,22 +8,6 @@ function App() {
   return (
     <Router>
       <Layout>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Routes>
           <Route path="/about" element={<AboutPage />} />
           <Route path="/" element={<HomePage />} />
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import { Link } from "react-router-dom"
 
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
@@ -11,12 +11,12 @@ const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">{AppConfig.APP_NAME}</Navbar.Brand>
+        <Navbar.Brand href="/">{AppConfig.APP_NAME}</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
             <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
