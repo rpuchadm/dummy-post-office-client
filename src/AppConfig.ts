64bd67a974d7fc302d3a78453549a55068017459
Rafael Puchades <rpuchadm@cfp.upv.es>
2024-12-09 21:11:35 +0100

    message

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,6 +1,6 @@
-
 const AppCoinfig = {
-    APP_NAME: "Dummy Post Office",
+  APP_NAME: "Dummy Post Office",
+  API_BASE_URL: "https://post.mydomain.com/post-office/",
 }
 
-export default AppCoinfig;
+export default AppCoinfig
--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,14 +1,65 @@
+import React, { useState } from "react"
+
+import Alert from "react-bootstrap/Alert"
 import Button from "react-bootstrap/Button"
 import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
-import Container from "react-bootstrap/esm/Container"
-import Row from "react-bootstrap/Row"
+import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
-import { FaEnvelope } from "react-icons/fa"
+import Row from "react-bootstrap/Row"
+import Spinner from "react-bootstrap/Spinner"
+import { FaEnvelope, FaExclamationTriangle } from "react-icons/fa"
+import AppCoinfig from "../../AppConfig"
 
 const Message = ({}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>("")
+  const [error, setError] = useState<string>("")
+  const [from, setFrom] = useState<string>("")
+  const [to, setTo] = useState<string>("")
+  const [subject, setSubject] = useState<string>("")
+  const [content, setContent] = useState<string>("")
+  const handleFrom = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFrom(e.target.value)
+  }
+  const handleTo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTo(e.target.value)
+  }
+  const handleSubject = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubject(e.target.value)
+  }
+  const handleContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value)
+  }
+  const handleSend = (ev) => {
+    ev.preventDefault()
+    setIsLoading(true)
+    const url = AppCoinfig.API_BASE_URL + "send"
+    const data = { from, to, subject, content }
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data)
+        const { message, ...otherdata } = data
+        if (message) {
+          setMessage(message)
+        }
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+        setError(error)
+        setIsLoading(false)
+      })
+  }
   return (
-    <Form>
+    <Form onSubmit={handleSend}>
       <Card>
         <Card.Header>Message</Card.Header>
         <Card.Body>
@@ -21,6 +72,8 @@ const Message = ({}) => {
                     type="email"
                     name="from"
                     placeholder="Enter email"
+                    value={from}
+                    onChange={handleFrom}
                   />
                 </Form.Group>
               </Col>
@@ -31,6 +84,8 @@ const Message = ({}) => {
                     type="email"
                     name="to"
                     placeholder="Enter email"
+                    value={to}
+                    onChange={handleTo}
                   />
                 </Form.Group>
               </Col>
@@ -43,6 +98,8 @@ const Message = ({}) => {
                     type="text"
                     name="subject"
                     placeholder="Subject"
+                    value={subject}
+                    onChange={handleSubject}
                   />
                 </Form.Group>
               </Col>
@@ -51,15 +108,39 @@ const Message = ({}) => {
               <Col>
                 <Form.Group className="mb-3" controlId="formContent">
                   <Form.Label>Content</Form.Label>
-                  <Form.Control as="textarea" name="content" rows={3} />
+                  <Form.Control
+                    as="textarea"
+                    name="content"
+                    rows={3}
+                    value={content}
+                    onChange={handleContent}
+                  />
                 </Form.Group>
               </Col>
             </Row>
           </Container>
         </Card.Body>
         <Card.Footer>
-          <Button>
-            <FaEnvelope /> Send
+          {error && (
+            <Alert variant="danger">
+              <FaExclamationTriangle size={20} /> {error}
+            </Alert>
+          )}
+          {message && (
+            <Alert variant="success">
+              <FaEnvelope size={20} /> {message}
+            </Alert>
+          )}
+          <Button
+            disabled={!!(isLoading || message || error)}
+            onClick={handleSend}
+          >
+            {isLoading ? (
+              <Spinner animation="border" size="sm" />
+            ) : (
+              <FaEnvelope />
+            )}{" "}
+            Send
           </Button>
         </Card.Footer>
       </Card>
