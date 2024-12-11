4e23a0f407e52263da0b0a80b3da689659174be3
Rafael Puchades <rpuchadm@cfp.upv.es>
2024-12-11 20:13:20 +0100

    k8s

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,6 +1,6 @@
 const AppConfig = {
   APP_NAME: "Dummy Post Office",
-  API_BASE_URL: "https://post.mydomain.com/post-office/",
+  API_BASE_URL: "https://post.mydomain.com/post-office-api/",
   TOKEN_ITEM_NAME: "post-office-token",
 }
 
